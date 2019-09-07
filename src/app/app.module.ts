6a356f173303de3b10e4bca43eeac2ea7501a1ef
Saggnic <saggnic1@gmail.com>
2019-09-07 18:26:44 +0530

    ProjectSetup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { ServerComponent } from "./server/server.component";
 import { AppComponent } from "./app.component";
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
